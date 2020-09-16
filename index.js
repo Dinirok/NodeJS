cde8402bb7d9e13b11fa95662fdd52f5f8dba7ff
Nikita Dolganov <dinirok3@yandex.ru>
2020-09-16 07:49:34 +0500

    Commit1

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express=require('express')
-const PORT=process.env.PORT || 3000
-
+const PORT=process.env.PORT || 3000  
 
 const app=express()
 app.listen(PORT,()  =>{
     console.log('Server has been started..')
+})
+app.get('/', function (req, res){
+    res.send('Hello')
 })
